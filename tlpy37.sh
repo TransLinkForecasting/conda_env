dccc2721caff2efbc3b114627759994c97bf3dec
Bo Wen <bo.wen@translink.ca>
2021-05-05 14:47:13 -0700

    add CryptPandas

--- tlpy37.sh
+++ tlpy37.sh
@@ -1,7 +1,9 @@
 # TL Python 3.7
+conda remove --name tlpy37 --all
 conda create -y -n tlpy37 python=3.7.6 pandas=1.0.2 numpy=1.18.1 scipy=1.4.1 matplotlib=3.1.3 seaborn=0.10.0 pyodbc=4.0.30 sqlalchemy=1.3.15 autopep8==1.5.4 xlrd xlsxwriter sympy nose scikit-learn pip ipykernel
 conda activate tlpy37
 conda install -c conda-forge mkl-service
+pip install CryptPandas==0.1.1
 ipython kernel install --user --name=tlpy37
 conda env export -n tlpy37 -f tlpy37.yml
 conda deactivate
