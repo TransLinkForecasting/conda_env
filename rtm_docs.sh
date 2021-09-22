782b9472fed1e0ab60520eee7f68a53df290aeb6
Bo Wen <bo.wen@translink.ca>
2021-09-22 15:55:16 -0700

    remove rtm_docs from environments
    please use instructions in README from doc repos

--- rtm_docs.sh
+++
@@ -1,6 +0,0 @@
-# mkdocs for RTM Docs
-conda remove --name rtm_docs --all
-conda create -y -n rtm_docs -c conda-forge python=3.6 pip=20.3.3 mkdocs==1.1.2
-conda activate rtm_docs
-conda env export -n rtm_docs -f rtm_docs.yml
-conda deactivate
