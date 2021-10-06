cdc40fed174a64502e5643b21a003e4097bd9578
Bo Wen <bo.wen@translink.ca>
2021-10-06 09:06:13 -0700

    update emat environment kernel name
    rename emat environment

--- emat_tmip.sh
+++ emat_tmip.sh
@@ -3,6 +3,6 @@
 conda install -n base -c defaults conda anaconda-client
 conda env create TMIP/EMAT
 conda activate EMAT
-ipython kernel install --user --name=popsim_master
-conda env export -n popsim_master -f popsim_master.yml
+ipython kernel install --user --name=EMAT
+conda env export -n EMAT -f EMAT.yml
 conda deactivate
