a802f120273e505679d39299ae11aa61129844b5
Bo Wen <bo.wen@translink.ca>
2021-10-12 13:31:27 -0700

    update abm_dev environment dependent packages

--- abm_dev.sh
+++ abm_dev.sh
@@ -15,14 +15,9 @@ conda install -y -n abm_dev -c conda-forge --override-channels mord=0.6 pylogit=
 pip install source/tlpytools/tlpytools-0.1.0-py3-none-any.whl --upgrade --force-reinstall
 # choice models
 pip install biogeme==3.2.8
-# # statsmodels pypi stable
-# pip install statsmodels==0.12.2
-# # statsmodels v0.13.0.dev0 (+686) - building from git requires c++ build tool
-# pip install git+https://github.com/statsmodels/statsmodels.git@v0.13.0.dev0 --upgrade
-# # statsmodels v0.13.0.dev0 (+686) from nightly channel
-# pip install -i https://pypi.anaconda.org/scipy-wheels-nightly/simple statsmodels --upgrade
-# statsmodels v0.13.0.dev0 (+686) from downloaded nightly channel wheel file (recommended)
-pip install source/statsmodel/statsmodels-0.13.0.dev0+20201204182244_127.g36b7f5ddf-cp38-none-win_amd64.whl --upgrade
+# statsmodels pypi stable
+pip install statsmodels==0.13.0
+# opionally, from nightly: https://anaconda.org/scipy-wheels-nightly/statsmodels/files
 # cryptography
 pip install CryptPandas==0.1.1
 # dash
@@ -31,7 +26,6 @@ pip install dash-leaflet==0.1.16
 pip install dash-extensions==0.0.58
 pip install jsbeautifier==1.14.0
 # ipython kernel
-pip install ipython_genutils
 ipython kernel install --user --name=abm_dev
 conda env export -n abm_dev -f abm_dev.yml
 conda deactivate
