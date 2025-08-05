1c979f3df061389e6458fd0fc323547a7a4a7e96
Bo Wen <56280490+bwentl@users.noreply.github.com>
2025-08-05 15:43:29 -0700

    rename abm_dev to asim1_2 and remove old unused abm related environments

--- asim1_2.sh
+++ asim1_2.sh
@@ -1,11 +1,11 @@
 ############################
 # ABM Development
 ############################
-mamba remove -y --name abm_dev --all
+conda remove -y --name asim1_2 --all
 # create development environment with asim dependencies installed (base, test, estimation, and notebooks)
-mamba create -y -n abm_dev -c conda-forge --override-channels python=3.9 "cytoolz>=0.8.1" "numba>=0.55.2" "numpy>=1.16.1,<=1.24" "openmatrix>=0.3.4.1" "orca>=1.6" "pandas>=1.1.0,<1.5" "psutil>=4.1" "pyarrow>=2.0" "pypyr>=5.3" "pyyaml>=5.1" "requests>=2.7" "scikit-learn>=1.1" "sharrow>=2.5" "simwrapper>1.7" "pytables>=3.5.1" "xarray>=0.21" larch statsmodels pytest pytest-cov coveralls pycodestyle pytest-regressions jupyterlab matplotlib descartes pandasql scipy seaborn pyodbc sqlalchemy openpyxl xlrd xlsxwriter sympy nose scikit-learn-intelex yapf pip ipykernel
+conda create -y -n asim1_2 -c conda-forge --override-channels python=3.9 "cytoolz>=0.8.1" "numba>=0.55.2" "numpy>=1.16.1,<=1.24" "openmatrix>=0.3.4.1" "orca>=1.6" "pandas>=1.1.0,<1.5" "psutil>=4.1" "pyarrow>=2.0" "pypyr>=5.3" "pyyaml>=5.1" "requests>=2.7" "scikit-learn>=1.1" "sharrow>=2.5" "simwrapper>1.7" "pytables>=3.5.1" "xarray>=0.21" larch statsmodels pytest pytest-cov coveralls pycodestyle pytest-regressions jupyterlab matplotlib descartes pandasql scipy seaborn pyodbc sqlalchemy openpyxl xlrd xlsxwriter sympy nose scikit-learn-intelex yapf pip ipykernel
 # activate environment
-mamba activate abm_dev
+conda activate asim1_2
 # # install activitysim
 # pip install "activitysim==1.2.1" --upgrade
 # optionally, override old versions of activitysim with development version
@@ -14,10 +14,10 @@ pip install https://github.com/TransLinkForecasting/conda_env/raw/master/source/
 # pip install source/popsim/populationsim-0.5.1.zip --upgrade
 # or install development version of populationsim
 pip install git+https://github.com/TransLinkForecasting/populationsim@master --upgrade
-# additional mamba packages, note:
+# additional conda packages, note:
 # - mord and pylogit - are for modeling
 # - contextily folium branca cycler kiwisolver - are for GIS
-mamba install -y -n abm_dev -c conda-forge --override-channels mord=0.6 pylogit=1.0.1 contextily folium branca cycler kiwisolver
+conda install -y -n asim1_2 -c conda-forge --override-channels mord=0.6 pylogit=1.0.1 contextily folium branca cycler kiwisolver
 # choice models
 pip install biogeme==3.2.11
 # cryptography
@@ -33,10 +33,10 @@ pip install polyline==2.0.2
 pip install tlpytools --upgrade
 pip install --upgrade "sqlalchemy<2.0"
 # ipython kernel
-ipython kernel install --user --name=abm_dev
+ipython kernel install --user --name=asim1_2
 # pip install jupyter_contrib_nbextensions
 # export environment
-mamba env export -n abm_dev -f abm_dev.yml --no-builds
+conda env export -n asim1_2 -f asim1_2.yml --no-builds
 # install geopandas precompiled wheels
 setx GDAL_VERSION "3.3.3"
 pip install source/gpd/GDAL-3.3.3-cp39-cp39-win_amd64.whl --upgrade
@@ -48,4 +48,4 @@ pip install source/gpd/Rtree-0.9.7-cp39-cp39-win_amd64.whl --upgrade
 # pip install source/gpd/rasterio-1.2.10-cp39-cp39-win_amd64.whl --upgrade
 pip install source/gpd/Cartopy-0.20.1-cp39-cp39-win_amd64.whl --upgrade
 # deactivate environment
-mamba deactivate
+conda deactivate
